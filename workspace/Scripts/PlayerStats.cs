1011b8ffe8146be0c2c1772bc4931214d5334781
steven.fisher2@gmail.com <VnoiSB0h>
2014-03-28 14:20:18 -0400

    PlayerStats
    code to store all information about the players statistics

---
+++ workspace/Scripts/PlayerStats.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System.Collections;
+ 
+public class CombatSystem : MonoBehaviour {
+
+    public CharacterController controller;
+    public int health = 1; 
+	public int lives = 3;
+	public bool PowerUp = false;
+	private float pTimeLeft = 5f;
+	
+	public GameObject respawn;
+	
+    void Start () {
+ 
+    }
+ 
+    void Update () {
+		DeadCheck ();
+	   
+		if (PowerUp && pTimeLeft > 0){
+			//Invincible
+			//KillsEnemies
+			//DoubleJump??
+			//Extra
+			//Reduce Timer
+		}
+    }
+ 
+    void onControllerColliderHit(ControllerColliderHit hit){
+		if (hit.gameObject.tag == "Enemy" && !PowerUp){
+			health = 0;
+		}
+    }
+	
+	void DeadCheck(){
+		if (health <= 0){
+			collider.enabled = false;
+			lives -= 1;
+			
+			if (lives <= 0){
+				Application.LoadLevel("GameOver");
+			} else {
+				Respawn ();
+			}
+		}
+	}
+	
+	void Respawn (){
+		respawn = GameObject.FindGameObjectWithTag("Resawn");
+		player.transform = respawn.transform
+		collider.enabled = true;
+	}
+}
